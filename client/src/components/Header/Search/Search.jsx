559dc355ab71ff94dc5e12d5ba9685f8683ccd89
Aman-Kumar778 <akm116655in@gmail.com>
2026-05-18 19:11:07 +0530

    feat: implement dynamic search querying Strapi API and nav on click

--- client/src/components/Header/Search/Search.jsx
+++ client/src/components/Header/Search/Search.jsx
@@ -1,26 +1,73 @@
-import "./Search.scss";
+import { useState, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
 import { MdClose } from "react-icons/md";
+import { fetchDataFromApi } from "../../../utils/api";
+import "./Search.scss";
 
-import prod from "../../../assets/products/earbuds-prod-1.webp";
 const Search = ({ setShowSearch }) => {
+  const [query, setQuery] = useState("");
+  const [data, setData] = useState(null);
+  const navigate = useNavigate();
+
+  const onChange = (e) => {
+    setQuery(e.target.value);
+  };
+
+  useEffect(() => {
+    if (!query.trim().length) {
+      setData(null);
+      return;
+    }
+
+    const timer = setTimeout(async () => {
+      const res = await fetchDataFromApi(
+        `/api/products?populate=*&filters[title][$containsi]=${query}`
+      );
+      setData(res);
+    }, 300);
+
+    return () => clearTimeout(timer);
+  }, [query]);
+
   return (
     <div className="search-modal">
       <div className="form-field">
-        <input type="text" autoFocus placeholder="Search for Products" />
+        <input
+          type="text"
+          autoFocus
+          placeholder="Search for Products"
+          value={query}
+          onChange={onChange}
+        />
         <MdClose onClick={() => setShowSearch(false)} />
       </div>
 
       <div className="search-result-content">
         <div className="search-results">
-          <div className="search-result-item">
-            <div className="img-container">
-              <img src={prod} alt="" />
+          {data?.data?.map((item) => (
+            <div
+              key={item.id}
+              className="search-result-item"
+              onClick={() => {
+                navigate("/product/" + item.id);
+                setShowSearch(false);
+              }}
+            >
+              <div className="img-container">
+                <img
+                  src={
+                    process.env.REACT_APP_DEV_URL +
+                    item.attributes.img.data[0].attributes.url
+                  }
+                  alt={item.attributes.title}
+                />
+              </div>
+              <div className="prod-details">
+                <span className="name">{item.attributes.title}</span>
+                <span className="desc">{item.attributes.desc}</span>
+              </div>
             </div>
-            <div className="prod-details">
-              <span className="name">product name</span>
-              <span className="desc">product desc</span>
-            </div>
-          </div>
+          ))}
         </div>
       </div>
     </div>
@@ -28,3 +75,4 @@ const Search = ({ setShowSearch }) => {
 };
 
 export default Search;
+
